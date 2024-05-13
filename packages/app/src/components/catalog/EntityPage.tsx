072f50c3f68b626c653fd3f5c4195f440a4b18b1
madalina.borota <madalina.borota@bertelsmann.de>
2024-05-13 11:33:15 +0300

    add grafana plugins

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -57,6 +57,7 @@ import {
 
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
+import { EntityGrafanaAlertsCard } from '@k-phoen/backstage-plugin-grafana';
 
 const techdocsContent = (
   <EntityTechdocsContent>
@@ -123,21 +124,23 @@ const entityWarningContent = (
 
 const overviewContent = (
   <Grid container spacing={3} alignItems="stretch">
-    {entityWarningContent}
-    <Grid item md={6}>
-      <EntityAboutCard variant="gridItem" />
-    </Grid>
-    <Grid item md={6} xs={12}>
-      <EntityCatalogGraphCard variant="gridItem" height={400} />
-    </Grid>
-
-    <Grid item md={4} xs={12}>
-      <EntityLinksCard />
-    </Grid>
-    <Grid item md={8} xs={12}>
-      <EntityHasSubcomponentsCard variant="gridItem" />
-    </Grid>
+  <Grid item md={6}>
+    <EntityAboutCard variant="gridItem" />
   </Grid>
+
+  <Grid item md={6}>
+    {/* Grafana alert card start */}
+    <EntityGrafanaAlertsCard />
+    {/* Grafana alert card end */}
+  </Grid>
+
+  <Grid item md={4} xs={12}>
+    <EntityLinksCard />
+  </Grid>
+  <Grid item md={8} xs={12}>
+    <EntityHasSubcomponentsCard variant="gridItem" />
+  </Grid>
+</Grid>
 );
 
 const serviceEntityPage = (
